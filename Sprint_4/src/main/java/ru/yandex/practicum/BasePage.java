a2c33cbb646064803908c3a0e2191124fcfda9a1
Uliana Limitovskaya <umlimitovs@mts.ru>
2023-06-18 19:49:50 +0300

    FinalProject_4

--- Sprint_4/src/main/java/ru/yandex/practicum/BasePage.java
+++ Sprint_4/src/main/java/ru/yandex/practicum/BasePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class BasePage {
     protected final WebDriver webDriver;
 
-    public BasePage(WebDriver webDriver) {
+    public  BasePage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 }
